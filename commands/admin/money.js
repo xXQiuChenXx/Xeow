1ebda6a5985738df63d2e70c6941d0a0c3bb0fc0
秋辰 <51996408+xXQiuChenXx@users.noreply.github.com>
2022-07-11 10:46:49 +0800

    update command

---
+++ commands/admin/money.js
@@ -0,0 +1,191 @@
+const Discord = require("discord.js");
+module.exports = {
+    config: {
+        name: "money",
+        description: "設置和更改錢幣指令",
+        usage: "money <give/take/set> <成員標註> <數量>",
+        memberPerms: ["ADMINISTRATOR"],
+        options: [
+            {
+                name: 'set',
+                type: 'SUB_COMMAND',
+                description: '設置成員的錢幣',
+                options: [
+                    {
+                        name: 'member',
+                        type: 'USER',
+                        description: '成員標註',
+                        required: true
+                    },
+                    {
+                        name: 'amount',
+                        type: 'NUMBER',
+                        description: '數量',
+                        required: true
+                    }
+                ]
+            },
+            {
+                name: 'give',
+                type: 'SUB_COMMAND',
+                description: '給予成員錢幣',
+                options: [
+                    {
+                        name: 'member',
+                        type: 'USER',
+                        description: '成員標註',
+                        required: true
+                    },
+                    {
+                        name: 'amount',
+                        type: 'NUMBER',
+                        description: '數量',
+                        required: true
+                    }
+                ]
+            },
+            {
+                name: 'take',
+                type: 'SUB_COMMAND',
+                description: '扣除成員的錢幣',
+                options: [
+                    {
+                        name: 'member',
+                        type: 'USER',
+                        description: '成員標註',
+                        required: true
+                    },
+                    {
+                        name: 'amount',
+                        type: 'NUMBER',
+                        description: '數量',
+                        required: true
+                    }
+                ]
+            }
+        ],
+    },
+    run: async (Xeow, message, args, config) => {
+        let type = args[0]
+        let member = message.mentions.users.first()
+        let amount = parseFloat(args[2])
+        if(!type) {
+            await Xeow.invalidUsage({
+                message: message,
+                arg: 0,
+                type: "empty"
+            })
+        }
+        if (!member) {
+            await Xeow.invalidUsage({
+                message: message,
+                arg: 1,
+                type: "empty"
+            })
+        }
+
+        if(amount === NaN) {
+            await Xeow.invalidUsage({
+                message: message,
+                arg: 2,
+                type: "empty"
+            })
+        }
+
+        if (amount < 0) return await Xeow.invalidUsage({
+            message: message,
+            arg: 2,
+            type: "incorrect",
+            reason: Xeow.translate("admin/money:cannot_negatif_number")
+        })
+
+        if (member.bot) return await Xeow.invalidUsage({
+            message: message,
+            arg: 1,
+            type: "incorrect",
+            reason: Xeow.translate("admin/money:cannot_negatif_number")
+        })
+
+        await Xeow.DBManager.sync()
+        let economy = Xeow.DBManager.get("economy")
+        let data = await economy.findOne({
+            where: {
+                guild: message.guild.id,
+                user: member.id
+            }
+        })
+
+        if (type === "give") {
+            let balance = data?.coins === undefined ? amount : parseFloat(data.coins) + amount
+            if (data?.user) {
+                await data.update({
+                    coins: balance
+                })
+                await data.save()
+            } else {
+                await economy.build({
+                    user: message.author.id,
+                    guild: message.guild.id,
+                    checked_in_count: 0,
+                    coins: balance
+                }).save();
+            }
+            const embed = new Discord.MessageEmbed()
+                .setColor(Xeow.translate("admin/money:give:color"))
+                .setTitle(Xeow.translate("admin/money:give:title"))
+                .setDescription(Xeow.translate("admin/money:give:description", {
+                    amount: amount.toFixed(2),
+                    member: member.toString(),
+                    balance: balance
+                }))
+            message.reply({ embeds: [embed] })
+        } else if (type === "take") {
+            let money = data?.coins === undefined ? 0 : data.coins
+            if (!data?.user || parseFloat(money) - amount < 0) return message.reply(Xeow.translate("admin/money:take:no_enough_money"))
+            let balance = parseFloat(data.coins) - amount
+            await data.update({
+                coins: balance
+            })
+            await data.save()
+
+            const embed = new Discord.MessageEmbed()
+                .setColor(Xeow.translate("admin/money:take:color"))
+                .setTitle(Xeow.translate("admin/money:take:title"))
+                .setDescription(Xeow.translate("admin/money:take:description", {
+                    amount: amount.toFixed(2),
+                    member: member.toString(),
+                    balance: balance
+                }))
+            message.reply({ embeds: [embed] })
+        } else if (type === "set") {
+            if (data?.user) {
+                await data.update({
+                    coins: amount
+                })
+                await data.save()
+            } else {
+                await economy.build({
+                    user: message.author.id,
+                    guild: message.guild.id,
+                    checked_in_count: 0,
+                    coins: amount
+                }).save();
+            }
+            const embed = new Discord.MessageEmbed()
+                .setColor(Xeow.translate("admin/money:set:color"))
+                .setTitle(Xeow.translate("admin/money:set:title"))
+                .setDescription(Xeow.translate("admin/money:set:description",
+                {
+                    member: member.toString(),
+                    amount: amount
+                }))
+            message.reply({ embeds: [embed] })
+        } else {
+            return await Xeow.invalidUsage({
+                message: message,
+                arg: 0,
+                type: "incorrect"
+            })
+        }
+    }
+}
---
+++ commands/economy/top.js
@@ -0,0 +1,47 @@
+const Discord = require("discord.js");
+module.exports = {
+    config: {
+        name: "top",
+        description: "金幣排行榜",
+        usage: "top"
+    },
+    run: async (Xeow, message, args, config) => {
+        await Xeow.DBManager.sync()
+        let data = await Xeow.DBManager.get("economy").findAll({
+            where: {
+                guild: message.guild.id
+            },
+            order: [
+                ['coins', "DESC"]
+            ]
+        })
+        let Embeds = []
+
+        // 1. 0-9   2. 10-19   3. 20-29
+        for (let i = 0; i <= parseInt(data.length / 10); i++) {
+            let embed = new Discord.MessageEmbed()
+                .setTitle(Xeow.translate("economy/top:money_top", {
+                    guild: message.guild.name
+                }))
+                .setDescription(data.slice(i * 10, i * 10 + 9).map((data, i) => {
+                    return `**${i + 1}.** <@${data.user}> - ${data.coins}`
+                }).join('\n'))
+                .setColor("RANDOM")
+            Embeds.push(embed)
+        }
+
+        if (message?.interaction) return await Xeow.Libraries.InteractionButtonPages({
+            interaction: message.interaction,
+            embeds: Embeds,
+            time: 60000,
+            end: true
+        })
+
+        await Xeow.Libraries.MessageButtonPages({
+            message: message,
+            embeds: Embeds,
+            time: 60000,
+            end: true
+        })
+    }
+}
